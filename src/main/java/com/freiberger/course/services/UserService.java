169f5e71f797d1597a3b62008b91f0cb7621d514
mfreiber <marcos.freiberger@capgemini.com>
2022-06-18 17:05:19 -0300

    Service layer, component registration

---
+++ src/main/java/com/freiberger/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.freiberger.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.freiberger.course.entities.User;
+import com.freiberger.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
