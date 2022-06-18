43dffeba3cf65297c8b349dea41d43a58ee0c1c6
mfreiber <marcos.freiberger@capgemini.com>
2022-06-18 17:56:44 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/freiberger/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.freiberger.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.freiberger.course.entities.Order;
+import com.freiberger.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findbyId(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
